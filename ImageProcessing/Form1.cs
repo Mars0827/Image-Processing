deb098b9f0f5af44fac3a73b6665a4e200abde56
Keiru <146981406+kwiruu@users.noreply.github.com>
2024-12-05 19:41:28 +0800

    Add files via upload

--- ImageProcessing/Form1.cs
+++ ImageProcessing/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ImageProcessing;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+
 
 namespace ImageProcessing
 {
@@ -148,6 +150,367 @@ namespace ImageProcessing
 
         private void ImageProcessor_Load(object sender, EventArgs e)
         {
+
+        }
+
+        private void smoothingToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            if (loaded == null)
+                return;
+
+            processed = new Bitmap(loaded);
+            BitmapFilter.Smooth(processed);
+            pictureBox2.Image = processed;
+
+        }
+
+        private void gaussianBlurToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (loaded == null)
+                return;
+
+            processed = new Bitmap(loaded);
+            BitmapFilter.GaussianBlur(processed);
+            pictureBox2.Image = processed;
+
+        }
+
+        private void blurToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (loaded == null)
+                return;
+
+            processed = new Bitmap(loaded);
+            BitmapFilter.GaussianBlur(processed);
+            pictureBox2.Image = processed;
+        }
+
+        private void embossToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (loaded == null)
+                return;
+
+            processed = new Bitmap(loaded);
+            BitmapFilter.EmbossAllDirections(processed);
+            pictureBox2.Image = processed;
+        }
+
+        private void sharpenToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (loaded == null)
+                return;
+
+            processed = new Bitmap(loaded);
+            BitmapFilter.Sharpen(processed);
+            pictureBox2.Image = processed;
+        }
+
+        private void shrinkToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (loaded == null)
+                return;
+
+            processed = new Bitmap(loaded);
+            BitmapFilter.Sharpen(processed);
+            pictureBox2.Image = processed;
+        }
+
+        private void meanRemovalToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (loaded == null)
+                return;
+
+            processed = new Bitmap(loaded);
+            BitmapFilter.MeanRemoval(processed);
+            pictureBox2.Image = processed;
+        }
+
+        private void lossyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (loaded == null)
+                return;
+
+            processed = new Bitmap(loaded);
+            BitmapFilter.EmbossLossy(processed);
+            pictureBox2.Image = processed;
+        }
+
+        private void horizOnlyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (loaded == null)
+                return;
+
+            processed = new Bitmap(loaded);
+            BitmapFilter.EmbossHorizontal(processed);
+            pictureBox2.Image = processed;
+        }
+
+        private void vertOnlyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (loaded == null)
+                return;
+
+            processed = new Bitmap(loaded);
+            BitmapFilter.EmbossVertical(processed);
+            pictureBox2.Image = processed;
+        }
+
+        private void embossLaplacianToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (loaded == null)
+                return;
+
+            processed = new Bitmap(loaded);
+            BitmapFilter.EmbossLaplacian(processed);
+            pictureBox2.Image = processed;
+        }
+
+        private void edgeDetectQuickToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (loaded == null)
+                return;
+
+            processed = new Bitmap(loaded);
+            BitmapFilter.EdgeDetectQuick(processed);
+            pictureBox2.Image = processed;
+        }
+
+        private void edgeDetectConvolutionToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (loaded == null)
+                return;
+
+            processed = new Bitmap(loaded);
+            BitmapFilter.EdgeDetectConvolution(processed, 1, 1);
+            pictureBox2.Image = processed;
+        }
+
+        private void edgeDetectHorizontalToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (loaded == null)
+                return;
+
+            processed = new Bitmap(loaded);
+            BitmapFilter.EdgeDetectHorizontal(processed);
+            pictureBox2.Image = processed;
+        }
+
+        private void edgeDetectVerticalToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (loaded == null)
+                return;
+
+            processed = new Bitmap(loaded);
+            BitmapFilter.EdgeDetectVertical(processed);
+            pictureBox2.Image = processed;
+        }
+
+        private void edgeDetectHomogenityToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (loaded == null)
+                return;
+
+            processed = new Bitmap(loaded);
+            BitmapFilter.EdgeDetectHomogenity(processed, 1);
+            pictureBox2.Image = processed;
+        }
+
+        private void edgeDetectDifferenceToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (loaded == null)
+                return;
+
+            processed = new Bitmap(loaded);
+            BitmapFilter.EdgeDetectDifference(processed, 1);
+            pictureBox2.Image = processed;
+        }
+
+        private void edgeEnhanceToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (loaded == null)
+                return;
+
+            processed = new Bitmap(loaded);
+            BitmapFilter.EdgeEnhance(processed, 1);
+            pictureBox2.Image = processed;
+        }
+
+        private void resizeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (loaded == null)
+                return;
+
+            processed = new Bitmap(loaded);
+            BitmapFilter.Resize(processed, 5, 5, true);
+            pictureBox2.Image = processed;
+        }
+
+        private void offsetFilterToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (loaded == null)
+                return;
+
+            processed = new Bitmap(loaded);
+            BitmapFilter.RandomJitter(processed, 1);
+            pictureBox2.Image = processed;
+        }
+
+        private void flipToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (loaded == null)
+                return;
+
+            processed = new Bitmap(loaded);
+            BitmapFilter.Flip(processed, true, true);
+            pictureBox2.Image = processed;
+        }
+
+        private void randomJitterToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (loaded == null)
+                return;
+
+            processed = new Bitmap(loaded);
+            BitmapFilter.RandomJitter(processed, 7);
+            pictureBox2.Image = processed;
+        }
+
+        private void swirlToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (loaded == null)
+                return;
+
+            processed = new Bitmap(loaded);
+            BitmapFilter.Swirl(processed, 5, true);
+            pictureBox2.Image = processed;
+        }
+
+        private void sphereToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (loaded == null)
+                return;
+
+            processed = new Bitmap(loaded);
+            BitmapFilter.Sphere(processed, true);
+            pictureBox2.Image = processed;
+        }
+
+        private void timeWrapToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (loaded == null)
+                return;
+
+            processed = new Bitmap(loaded);
+            BitmapFilter.TimeWarp(processed, 5, true);
+            pictureBox2.Image = processed;
+        }
+
+        private void moireToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (loaded == null)
+                return;
+
+            processed = new Bitmap(loaded);
+            BitmapFilter.Moire(processed, 1);
+            pictureBox2.Image = processed;
+        }
+
+        private void waterToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (loaded == null)
+                return;
+
+            processed = new Bitmap(loaded);
+            BitmapFilter.Water(processed, 1, true);
+            pictureBox2.Image = processed;
+        }
+
+        private void pixelateToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (loaded == null)
+                return;
+
+            processed = new Bitmap(loaded);
+            BitmapFilter.Pixelate(processed, 2, true);
+            pictureBox2.Image = processed;
+        }
+
+        private void hEHEToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            processed = new Bitmap(loaded.Width, loaded.Height);
+            Color pixel, gray, inv;
+
+            for (int x = 0; x < loaded.Width / 2; x++)
+            {
+                for (int y = 0; y < loaded.Height / 2; y++)
+                {
+
+                    pixel = loaded.GetPixel(x, y);
+                    inv = Color.FromArgb(255 - pixel.R, 255 - pixel.G, 255 - pixel.B);
+                    processed.SetPixel(x, y, inv);
+
+                }
+            }
+
+            int grey;
+            for (int x = loaded.Width / 2; x < loaded.Width; x++)
+            {
+                for (int y = 0; y < loaded.Height / 2; y++)
+                {
+
+                    pixel = loaded.GetPixel(x, y);
+                    grey = (int)(pixel.R + pixel.G + pixel.B) / 3;
+                    gray = Color.FromArgb(grey, grey, grey);
+                    processed.SetPixel(x, y, gray);
+
+                }
+            }
+
+            for (int x = 0; x < loaded.Width / 2; x++)
+            {
+                for (int y = loaded.Height / 2; y < loaded.Height; y++)
+                {
+
+                    pixel = loaded.GetPixel(x, y);
+                    processed.SetPixel(x, y, pixel);
+
+                }
+            }
+
+            for (int x = loaded.Width / 2; x < loaded.Width; x++)
+            {
+                for (int y = loaded.Height / 2; y < loaded.Height; y++)
+                {
+
+                    pixel = loaded.GetPixel(x, y);
+                    processed.SetPixel(x, loaded.Height - y + loaded.Height / 2 - 1, pixel);
+
+                }
+            }
+
+            pictureBox2.Image = processed;
+        }
+
+        private void goToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            loaded = new Bitmap(openFileDialog1.FileName);
+
+            bool result = BitmapFilter.EdgeDetectConvolution(loaded, 1, 100);
+
+            int coinCount = BitmapFilter.CountBlobs(loaded);
+
+            Bitmap withText = new Bitmap(loaded);
+            using (Graphics g = Graphics.FromImage(withText))
+            {
+                Font font = new Font("Arial", 24, FontStyle.Bold);
+                Brush brush = new SolidBrush(Color.Red);
+                PointF position = new PointF(10, 10);
+
+                g.DrawString($"Coins: {coinCount}", font, brush, position);
+            }
+
+            pictureBox2.Image = withText;
 
         }
 
